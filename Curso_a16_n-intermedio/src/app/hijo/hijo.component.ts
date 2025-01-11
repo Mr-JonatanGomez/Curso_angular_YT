c6a6fe74ee517baebdff8a8eefe63f69d90d8917
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2025-01-11 15:54:50 +0100

    A17-Info entreComponentes

--- Curso_a16_n-intermedio/src/app/hijo/hijo.component.ts
+++ Curso_a16_n-intermedio/src/app/hijo/hijo.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hijo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './hijo.component.html',
   styleUrl: './hijo.component.css'
 })
 export class HijoComponent {
+
+  //se iguaña a vacio, porque lo va a coger del padre
+  /* @Input()recibeHijo:string = "" */
+
+  // tambien podemos no iniciar a vacio y darselo como opcion a vacio con ?
+  @Input()recibeHijo?:string
+
+  // OUTPUT
+  @Output() mensajeDesdeHijo = new EventEmitter<string>()
+  @Output() incrementarDesdeHijo = new EventEmitter<void>()
+  @Output() decrementarDesdeHijo = new EventEmitter<void>()
+
+  mensaje?:string
+
+  enviarMensaje(){
+    this.mensajeDesdeHijo.emit(this.mensaje)
+  }
+
+  incrementar(){
+    this.incrementarDesdeHijo.emit()
+  }
+  decrementar(){
+    this.decrementarDesdeHijo.emit()
+  }
+
 
 }
--- Curso_a16_n-intermedio/src/app/padre/padre.component.ts
+++ Curso_a16_n-intermedio/src/app/padre/padre.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { HijoComponent } from "../hijo/hijo.component";
 
 @Component({
   selector: 'app-padre',
   standalone: true,
-  imports: [],
+  imports: [HijoComponent],
   templateUrl: './padre.component.html',
   styleUrl: './padre.component.css'
 })
 export class PadreComponent {
+
+  valorContador: number=0
+  //INPUT
+  mensajePadre = "Mensaje desde el padre, AUPA ATLETI"
+
+
+  //OUTPUT , se crea atributo, y function con param de evento:string
+  mensajeRecibido ?:string // ? en lugar de igualar a vacio el string
+
+  recibirMensaje($event: string){
+    this.mensajeRecibido=$event
+  }
+
+
+  incrementar(){
+    this.valorContador ++
+  }
+  decrementar(){
+    this.valorContador --
+  }
+
 
 }
